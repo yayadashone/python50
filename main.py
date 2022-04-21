e3156837547b76f62e8ed8fbe3466806f4e0dd50
estherhuang <yayadashone@gmail.com>
2022-04-21 12:45:23 +0800

    new

---
+++ main.py
@@ -0,0 +1,5 @@
+print('hello world')
+
+import script1
+
+script1.writeMessage('This is called from main.py')
---
+++ script1.py
@@ -0,0 +1,6 @@
+def writeMessage(message):
+    print('Message received:', message)
+    return
+
+if __name__ == '__main__':
+    writeMessage('This is called from script1.py')
